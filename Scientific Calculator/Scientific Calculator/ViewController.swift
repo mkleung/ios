4bd1a0becdb54756ad5efdfc1b42d6df254b045f
Mike Leung <mpakleung@gmail.com>
2020-07-14 14:34:08 -0400

    first commit

---
+++ Scientific Calculator/Scientific Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scientific Calculator
+//
+//  Created by mike on 2020-07-14.
+//  Copyright © 2020 mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
